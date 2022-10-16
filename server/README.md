452121793f51d0f205e6a57b2b99eaa5f788fd7b
adityakasralkar <adityakasralkar@gmail.com>
2022-10-16 17:20:45 +0530

    Completed Setup of Server & Added 'Babel' to it, Made Mongo Connection

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# API Planing
+
+ - Food ( Food items and their Details)
+ - Restaurant ( Restaurant and their Details)
+ - Menu (Menu and its details)
+ - Order (Order and its details)
+ - Images (Storing all the images related to the project)
+ - Review collection (All the list of reviews)
+ - User (User related details ... => Username , email , Password)
+ 
---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv'; 
+
+// Database Connection
+import ConnectDB from './database/connection';
+
+
+
+dotenv.config();
+
+const zomato = express();
+
+zomato.use(express.json());
+
+zomato.get('/' , (req , res ) => {
+    res.json({
+        message: "Server is Running",
+    });
+});
+
+const PORT = 4000;
+
+zomato.listen(PORT , () => {
+    // ConnectDB()
+    // .then(()=> {
+    //     console.log("Server is Running..!!");
+    // })
+    // .catch((error) => {
+    //     console.log("Server is Running  , But Database Connection Failed..");
+    //     console.log(error);
+    // })
+
+    console.log("Server is Running..!!");
+})
